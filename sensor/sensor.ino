49ffa77d748bfea3d083aae5212d419663b060fc
6899Viraj <virajhapaliya25@gmail.com>
2018-03-03 14:32:47 +0530

    new

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,21 +1,70 @@
-void setup() {
-  pinMode(3,INPUT); //three ir sensors will be placed over herecc
-   pinMode(4,INPUT);
-    pinMode(5,INPUT);
+int ir_sesnor1 = 4;
+int ir_sesnor2 = 5;
+int ir_sesnor3 = 13;
 
-  pinMode(6,OUTPUT); //these three pins are there for the triggering signal for display indication or for deploying the airbag 
-    pinMode(7,OUTPUT);
-      pinMode(8,OUTPUT);
-  
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(ir_sesnor1,INPUT);
+pinMode(ir_sesnor2,INPUT);
+pinMode(ir_sesnor3,INPUT);
+pinMode(7,OUTPUT);
+pinMode(8,OUTPUT);
+pinMode(9,OUTPUT);
+
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
 
-int s1=digitalRead(3);
-int s2=digitalRead(4);
-int s3=digitalRead(5);
+int a = digitalRead(ir_sesnor1);
+int b = digitalRead(ir_sesnor2);
+int c = digitalRead(ir_sesnor3);
 
-  
-//added emoved
+Serial.print(a);Serial.print(" ");
+Serial.print(b);Serial.print(" ");
+Serial.println(c);Serial.print(" ");
+
+
+if(c==HIGH)
+{
+  digitalWrite(8,HIGH);
+  digitalWrite(9,HIGH);
+  delay(1000);
+  digitalWrite(8,LOW);
+  digitalWrite(9,LOW);
+  }
+
+if(b==HIGH)
+{
+  digitalWrite(8,HIGH);
+  //digitalWrite(9,HIGH);
+  delay(1000);
+  digitalWrite(8,LOW);
+  //digitalWrite(9,LOW);
+  }
+if(a==HIGH)
+{
+  digitalWrite(8,HIGH);
+  digitalWrite(7,HIGH);
+  delay(1000);
+  digitalWrite(8,LOW);
+  digitalWrite(7,LOW);
+  }
+if((a==HIGH && c==HIGH)||(a==HIGH && c==HIGH && b==HIGH))
+{
+  digitalWrite(7,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(9,HIGH);
+  delay(1000);
+  digitalWrite(7,LOW);
+  digitalWrite(8,LOW);
+  digitalWrite(9,LOW);
+  }
+
+
+
+
 
 }
