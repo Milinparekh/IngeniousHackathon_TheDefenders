aa7358ddafeb1a126b2efb40c092265a0dbebca2
Milinparekh <milinparekh93@gmail.com>
2018-03-03 13:15:28 +0530

    Add files via upload

---
+++ sensor.ino
@@ -0,0 +1,21 @@
+void setup() {
+  pinMode(3,INPUT); //three ir sensors will be placed over herecc
+   pinMode(4,INPUT);
+    pinMode(5,INPUT);
+
+  pinMode(6,OUTPUT); //these three pins are there for the triggering signal for display indication or for deploying the airbag 
+    pinMode(7,OUTPUT);
+      pinMode(8,OUTPUT);
+  
+}
+
+void loop() {
+
+int s1=digitalRead(3);
+int s2=digitalRead(4);
+int s3=digitalRead(5);
+
+  
+
+
+}
